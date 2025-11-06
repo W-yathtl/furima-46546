b1348c69f4f7dbc01b7290d984451f3adbd6eecd
W-yathtl <dopin9panda@gmail.com>
2025-11-06 17:33:48 +0900

    削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
  # ルートパスにアクセスしてもindexメソッドでトップページにアクセスするように設定
   root to: 'items#index'
 
-    resources :items, except: [:destroy] do
+    resources :items do
     resources :orders, only: [:index, :create]
   end
 end
