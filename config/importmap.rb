cee475ff3f7e30c6bf5fa9821a4a0e0147ade546
W-yathtl <dopin9panda@gmail.com>
2025-11-07 18:29:58 +0900

    importmapのインストール

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,5 @@
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+# Pin npm packages by running ./bin/importmap
 
-pin "card", to: "card.js"
+pin "application"
 pin "item_price", to: "item_price.js"
+pin "card", to: "card.js"
