d3de26a4b07a79b3d70012998de64ee1da670bdc
W-yathtl <dopin9panda@gmail.com>
2025-11-12 17:07:23 +0900

    cgi2

--- config/initializers/cgi_hotfix.rb
+++ config/initializers/cgi_hotfix.rb
@@ -1,2 +1,5 @@
 require "cgi"
-CGI.class_variable_set(:@@accept_charset, nil) unless CGI.class_variable_defined?(:@@accept_charset)
+
+unless CGI.class_variable_defined?(:@@accept_charset)
+  CGI.class_variable_set(:@@accept_charset, nil)
+end
