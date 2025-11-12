de92dd8b3b06caa3ae565dcdb40088ae45537f97
W-yathtl <dopin9panda@gmail.com>
2025-11-12 17:30:45 +0900

    コミット5

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,9 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+require 'cgi'
+begin
+  CGI.accept_charset = 'UTF-8'
+rescue NameError
+  CGI.send(:class_variable_set, :@@accept_charset, 'UTF-8')
+end
