6a494d46350b8a4eb4103dcff06c0a3b94afda12
W-yathtl <dopin9panda@gmail.com>
2025-11-12 17:25:54 +0900

    コミット4

--- config/initializers/cgi_hotfix.rb
+++ config/initializers/cgi_hotfix.rb
@@ -1,7 +1,6 @@
 require "cgi"
 
 CGI.class_eval do
-  remove_class_variable(:@@accept_charset) if class_variable_defined?(:@@accept_charset)
-  class_variable_set(:@@accept_charset, nil)
+  class_variable_set(:@@accept_charset, nil) unless class_variable_defined?(:@@accept_charset)
 end
 CGI.unescape("init")
